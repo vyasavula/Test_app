5377ab916093fc105d29faa7f8b5d40808a98094
vyasavula <vyasavula6@gmail.com>
2017-11-14 10:05:56 +0000

    create new template, action and route

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
 root 'welcome#home'
 get 'about', to: 'welcome#about'
+resources :articles
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
