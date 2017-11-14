131ce2676e746ebed85256d4f2b85b03359c2873
vyasavula <vyasavula6@gmail.com>
2017-11-14 13:18:59 +0000

    Limits in the cahracters update

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
-  validates :title ,presence: true,length: {minimum:20, maximum:50}
+  validates :title ,presence: true,length: {minimum:3, maximum:10}
 
-  validates :description, presence: true, length: {minimum:20, maximum:40}
+  validates :description, presence: true, length: {minimum:20, maximum:200}
   
 end
