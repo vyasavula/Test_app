2c1168332a9d2de61285bfd92e10da99757a4983
vyasavula <vyasavula6@gmail.com>
2017-11-28 06:19:10 +0000

    Add association between the user and aricle and add the debugger view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,7 @@ end
 def create
   #render plain: params[:article].inspect
   @article=Article.new(article_params)
+  @article.user= User.first
   if @article.save
     flash[:success] = "Article was created successfully"
   redirect_to article_path(@article)
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ActiveRecord::Base
-  validates :title ,presence: true,length: {minimum:3, maximum:10}
-
-  validates :description, presence: true, length: {minimum:20, maximum:200}
+  belongs_to :user 
+  validates :title ,presence: true,length: {minimum:3, maximum:40}
+  validates :description, presence: true, length: {minimum:20, maximum:2000}
+  validates :user_id, presence: true
   
 end
