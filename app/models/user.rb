cce024d790bc98e4128615628c621955dd41876c
vyasavula <vyasavula6@gmail.com>
2017-11-28 08:30:50 +0000

    add secure password and attribute to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ActiveRecord::Base
          VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum:105}, 
              uniqueness: {case_senstive:false}, format: {with: VALID_EMAIL_REGEX}
-  
+  has_secure_password
   
 end
