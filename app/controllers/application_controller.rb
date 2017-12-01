fcb37d5bd3f787927dcf0a39d2fb1b0d510cc6ab
vyasavula <vyasavula6@gmail.com>
2017-12-01 09:32:07 +0000

    Add sign in upon sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   def current_user
  @current_user || 
   User.find(session[:user_id]) if session[:user_id]
-end
+  end
 
 def logged_in?
   !!current_user
@@ -19,7 +19,7 @@ def require_user
   if !logged_in?
   flash[:danger]= "You must be logged in to perform that action"
   redirect_to root_path
-end
+  end
   
 end
 end
